48cd3337d0cfe6572d0f350fe74f2cb22a8aa1f8
Robert O <romohund@gmail.com>
2019-03-02 20:43:23 -0600

    Complete code with a couple of bugs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,20 @@
 var newFriends = [
     {
-        name: "Robert",
-        photo: "",
-        scores: []
+        name: "My (evil?) twin",
+        photo: "/images/evil-twins.jpg",
+        scores: [5, 3, 4, 4, 3, 4, 3, 2, 5, 5]
+
+    }, 
+    {
+        name: "Mario",
+        photo: "/images/Mario.png",
+        scores: [2, 5, 5, 3, 4, 1, 3, 5, 5, 3]
+
+    }, 
+    {
+        name: "Bowser",
+        photo: "/images/bowser.png",
+        scores: [5, 4, 2, 1, 5, 2, 1, 1, 4, 2]
 
     }
 ];
